7d1e62ae2e2c207443969c600d24af378cac1587
leongaban <leongaban@gmail.com>
2022-07-05 11:49:29 +0800

    added cypress + init cypress setup

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The rest of the Cypress config options go here...
+  projectId: 'qo55ed',
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
