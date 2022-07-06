721511a903659520659e7986bccf0dfe4a4fba43
leongaban <leongaban@gmail.com>
2022-07-07 00:02:18 +0800

    writing AccountSiderBar tests

--- src/components/Accounts/AccountsSideBar.test.jsx
+++ src/components/Accounts/AccountsSideBar.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import AccountsSideBar from './AccountsSideBar';
 
 describe('The AccountsSideBar component', () => {
-  test('containes expected classes', () => {
+  test('contains expected classes and text', () => {
     const socialAccounts = [
       {
         name: 'FB account',
@@ -21,5 +21,9 @@ describe('The AccountsSideBar component', () => {
     expect(container.getElementsByClassName('sidebar').length).toBe(1);
     expect(container.getElementsByClassName('select-all').length).toBe(1);
     expect(container.getElementsByClassName('checkbox').length).toBe(2);
+
+    expect(screen.getByText('Select All Accounts')).toBeVisible();
+    expect(screen.getByText('FB account')).toBeVisible();
+    expect(screen.getByText('IG account')).toBeVisible();
   });
 });
