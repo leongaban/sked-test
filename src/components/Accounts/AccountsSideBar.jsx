dd27904fc16b13e166fea4f5d291fb2be372880a
leongaban <leongaban@gmail.com>
2022-07-07 09:56:42 +0800

    all cypress tests passing

--- src/components/Accounts/AccountsSideBar.jsx
+++ src/components/Accounts/AccountsSideBar.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Circle from '../partials/Circle.jsx';
 
 const AccountsSideBar = ({ accounts, filterPosts }) => {
+  console.log('accounts', accounts);
   // Redux solution:
   // const accounts = useSelector((state) => state.Accounts);
 
@@ -49,9 +50,14 @@ const AccountsSideBar = ({ accounts, filterPosts }) => {
 
   const listAccounts = (accts) =>
     accts.map((acct, i) => (
-      <li key={i}>
+      <li key={i} className="social-media-account-toggle">
         <div className="checkbox">
-          <input type="checkbox" onChange={() => toggleCheck(acct.socialMedia)} checked={checked[acct.socialMedia]} />
+          <input
+            type="checkbox"
+            id={`check${i}`}
+            onChange={() => toggleCheck(acct.socialMedia)}
+            checked={checked[acct.socialMedia]}
+          />
         </div>
         <Circle initial={acct.initial} name={acct.name} />
         <p>{acct.name}</p>
@@ -61,7 +67,7 @@ const AccountsSideBar = ({ accounts, filterPosts }) => {
   return (
     <main className="sidebar">
       <div className="select-all">
-        <input type="checkbox" onChange={(event) => selectAll(event.target.checked)} checked={checkedAll} />
+        <input type="checkbox" id="check-all" onChange={(event) => selectAll(event.target.checked)} checked={checkedAll} />
         <p>Select All Accounts</p>
       </div>
       <div>{listAccounts(accounts)}</div>
